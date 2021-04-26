9f9eabdc31fab0de8bbadab74b11f57319c8ab7a
Declan Tao <442472147@qq.com>
2021-04-26 15:00:45 -0400

    change etc/environment format2

--- src/main/java/shortvideo/declantea/me/config/HibernateConfig.java
+++ src/main/java/shortvideo/declantea/me/config/HibernateConfig.java
@@ -21,6 +21,18 @@ public class HibernateConfig {
 
     private ApplicationContext context;
 
+    @Value("${DB_ENDPOINT}")
+    private String db_endpoint;
+
+    @Value("${DB_DATABASE}")
+    private String db_database;
+
+    @Value("${DB_USERNAME}")
+    private String db_username;
+
+    @Value("${DB_PASSWORD}")
+    private String db_password;
+
     private static final Logger logger= LoggerFactory.getLogger(HibernateConfig.class);
 
     @Autowired
@@ -32,10 +44,10 @@ public class HibernateConfig {
     @Bean
     public LocalSessionFactoryBean getSessionFactory() {
         LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-        sessionFactory.setConfigLocation(context.getResource("classpath:hibernate-cfg/hibernate.cfg.xml"));
-//        sessionFactory.setHibernateProperties(getHibernateProperties());
+//        sessionFactory.setConfigLocation(context.getResource("classpath:hibernate-cfg/hibernate.cfg.xml"));
+        sessionFactory.setHibernateProperties(getHibernateProperties());
 //        sessionFactory.setDataSource(dataSource());
-//        logger.debug(sessionFactory.getHibernateProperties().toString());
+        logger.debug(sessionFactory.getHibernateProperties().toString());
         sessionFactory.setPackagesToScan("shortvideo.declantea.me.entity");
         return sessionFactory;
     }
@@ -47,24 +59,24 @@ public class HibernateConfig {
         return transactionManager;
     }
 
-//    @Bean
-//    public Properties getHibernateProperties(){
-//        Properties properties = new Properties();
-//        properties.put(Environment.DRIVER,"com.mysql.cj.jdbc.Driver");
-//        logger.debug("DB_ENDPOINT:{}",System.getenv("DB_ENDPOINT"));
-//        properties.put(Environment.URL,"jdbc:mysql://"+System.getenv("DB_ENDPOINT")+"/"+System.getenv("DB_DATABASE")+"?createDatabaseIfNotExist=true");
-//        logger.debug("DB_UserName:{}",System.getenv(""));
-//        properties.put(Environment.USER,System.getenv("DB_USERNAME"));
-//        properties.put(Environment.PASS,System.getenv("DB_PASSWORD"));
-//        properties.put("show_sql",true);
-//        properties.put("hibernate.dialect","org.hibernate.dialect.MySQL8Dialect");
-//        properties.put("hibernate.connection.autocommit",true);
-//        properties.put("hibernate.c3p0.min_size",5);
-//        properties.put("hibernate.c3p0.max_size",20);
-//        properties.put("hibernate.c3p0.timeout",1800);
-//        properties.put("hibernate.c3p0.max_statements",150);
-//        properties.put("hibernate.hbm2ddl.auto","create");
-//        return properties;
-//    }
+    @Bean
+    public Properties getHibernateProperties(){
+        Properties properties = new Properties();
+        properties.put(Environment.DRIVER,"com.mysql.cj.jdbc.Driver");
+        logger.debug("DB_ENDPOINT:{}",db_endpoint);
+        properties.put(Environment.URL,"jdbc:mysql://"+db_endpoint+"/"+db_database+"?createDatabaseIfNotExist=true");
+        logger.debug("DB_UserName:{}",db_username);
+        properties.put(Environment.USER,db_username);
+        properties.put(Environment.PASS,db_password);
+        properties.put("show_sql",true);
+        properties.put("hibernate.dialect","org.hibernate.dialect.MySQL8Dialect");
+        properties.put("hibernate.connection.autocommit",true);
+        properties.put("hibernate.c3p0.min_size",5);
+        properties.put("hibernate.c3p0.max_size",20);
+        properties.put("hibernate.c3p0.timeout",1800);
+        properties.put("hibernate.c3p0.max_statements",150);
+        properties.put("hibernate.hbm2ddl.auto","create");
+        return properties;
+    }
 
 }

7f3e1039c1d055b7ce92f8d6e5826fef098ca627
Declan Tao <442472147@qq.com>
2021-04-29 07:48:43 -0400

    change way to read hibernate properties

--- src/main/java/shortvideo/declantea/me/config/HibernateConfig.java
+++ src/main/java/shortvideo/declantea/me/config/HibernateConfig.java
@@ -21,6 +21,18 @@ public class HibernateConfig {
 
     private ApplicationContext context;
 
+    @Value("${DB_ENDPOINT}")
+    private String db_endpoint;
+
+    @Value("${DB_DATABASE}")
+    private String db_database;
+
+    @Value("${DB_USERNAME}")
+    private String db_username;
+
+    @Value("${DB_PASSWORD}")
+    private String db_password;
+
 
     private static final Logger logger= LoggerFactory.getLogger(HibernateConfig.class);
 
@@ -33,8 +45,8 @@ public class HibernateConfig {
     @Bean
     public LocalSessionFactoryBean getSessionFactory() {
         LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-        sessionFactory.setConfigLocation(context.getResource("classpath:hibernate-cfg/hibernate.cfg.xml"));
-//        sessionFactory.setHibernateProperties(getHibernateProperties());
+//        sessionFactory.setConfigLocation(context.getResource("classpath:hibernate-cfg/hibernate.cfg.xml"));
+        sessionFactory.setHibernateProperties(getHibernateProperties());
 //        sessionFactory.setDataSource(dataSource());
         logger.debug("ENV:{}",System.getenv());
         sessionFactory.setPackagesToScan("shortvideo.declantea.me.entity");
@@ -48,5 +60,22 @@ public class HibernateConfig {
         return transactionManager;
     }
 
+    @Bean
+    public Properties getHibernateProperties(){
+        Properties properties = new Properties();
+        properties.put(Environment.DRIVER,"com.mysql.cj.jdbc.Driver");
+        properties.put(Environment.URL,"jdbc:mysql://"+db_endpoint+"/"+db_database+"?createDatabaseIfNotExist=true");
+        properties.put(Environment.USER,db_username);
+        properties.put(Environment.PASS,db_password);
+        properties.put("show_sql",true);
+        properties.put("hibernate.dialect","org.hibernate.dialect.MySQL8Dialect");
+        properties.put("hibernate.connection.autocommit",true);
+        properties.put("hibernate.c3p0.min_size",5);
+        properties.put("hibernate.c3p0.max_size",20);
+        properties.put("hibernate.c3p0.timeout",1800);
+        properties.put("hibernate.c3p0.max_statements",150);
+        properties.put("hibernate.hbm2ddl.auto","create");
+        return properties;
+    }
 
 }
